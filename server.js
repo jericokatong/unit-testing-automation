c3c560feced02d8de03026be5bb9e5a0e2dd2f2b
jericokatong <jericokatong@gmail.com>
2024-04-16 12:50:29 +0800

    update: server 11

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { sumFunction } = require('./utils');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server is running 10');
+  res.send('Server is running 11');
 });
 
 app.post('/sum', (req, res) => {
