4f9a9e7cc21518f88255b346938a848e56d8a616
jericokatong <jericokatong@gmail.com>
2024-04-16 11:08:32 +0800

    update: wrong testing

--- unit.test.js
+++ unit.test.js
@@ -2,6 +2,6 @@ const { sumFunction } = require('./utils');
 
 describe('sum function', () => {
   test('when given value1 and value2, it should return the sum of these two values', () => {
-    expect(sumFunction(1, 2)).toEqual(3);
+    expect(sumFunction(1, 2)).toEqual(5);
   });
 });
